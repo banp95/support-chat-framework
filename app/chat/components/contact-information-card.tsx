118bd0808941bb0a51ea2bffdbf8dc7aa5f3dc15
brayanpesantes <brayalexis.30@gmail.com>
2025-04-18 12:41:09 -0500

    Refactor ContactInformationCard and ContactInformation components to utilize client data from props and improve rendering logic for selected clients.

--- app/chat/components/contact-information-card.tsx
+++ app/chat/components/contact-information-card.tsx
@@ -1,9 +1,19 @@
+import { useLoaderData, useNavigation, useParams } from "react-router";
+import type { Client } from "~/interfaces/chat.interface";
 import { ContactInformation } from "./contact-information";
 import { ContactInformationSkeleton } from "./contact-information-skeleton";
 import { NotContactSelected } from "./not-contact-selected";
 
 export const ContactInformationCard = () => {
-  return <NotContactSelected />;
-  return <ContactInformationSkeleton />;
-  return <ContactInformation />;
+  const { id } = useParams();
+  const { clients = [] } = useLoaderData();
+  const { state } = useNavigation();
+
+  const isPending = state === "loading";
+
+  if (isPending) return <ContactInformationSkeleton />;
+  if (!id) return <NotContactSelected />;
+  const client = clients.find((client: Client) => client.id === id);
+  if (!client) return <NotContactSelected />;
+  return <ContactInformation client={client} />;
 };
